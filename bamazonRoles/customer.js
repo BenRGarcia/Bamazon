d9982182431131af60cc9afe92c0102d9b26e7c1
Ben Garcia <ben@seebenprogram.com>
2018-04-04 14:34:13 -0400

    Add code for testing Database class

--- bamazonRoles/customer.js
+++ bamazonRoles/customer.js
@@ -1,3 +1,9 @@
 let Database = require('../database/database.js');
 
 let db = new Database();
+
+db.getAllProducts().then( response => {
+  console.log(`\nResponse in customer.js:\n`);
+  console.log(response);
+  db.disconnect();
+});

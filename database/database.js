e4509671a9942c047f7decab47849569e06a81c9
Ben Garcia <ben@seebenprogram.com>
2018-04-05 17:23:58 -0400

    Delete extra comments, build private methods

--- database/database.js
+++ database/database.js
@@ -13,69 +13,44 @@ const _connection = mysql.createConnection({
   database: keys.mysql.db_name,
 });
 
-/**
- *  Thoughts:
- *    1) Effective encapsulation/information hiding, absolutely minimized public interface
- *        * Rely on ES6 module syntax? (put 'private members' in module's global scope and don't export)
- *        * Let constructor be a little wasteful with memory?
- *        * Create a bridge/switch statement to route prototypal method calls to private/priviledged members
- *        * Should I emulate C "Interfaces" to test implementations on params passed?
- *    2) Open/Closed Principle
- *        * How far to go with closed to modification (Object.seal/freeze/defineProperty - read/write/enum/config)
- *    3) How to minimize DB round trips
- *        * feels inefficienct -> 1) retrieve initial data, process data based on current value,
- *                                2) post new/updated data, 
- *                                3) get DB response absent errors
- *                                4) again retrieve data to actually see new info
- *
- */
-
 class Database {
 
   constructor() {
 
-    // Establish connection with database upon instantiation
-    _connection.connect(err => {
-      if (err) throw err;
-      console.log(`MySQL connected as id: ${_connection.threadId}`);
-    });
-
     /**
-     *  Private members (Disadvantage = memory -> a copy made in each instantiation)
+     *  Private members: Immutable/read only via predefined class methods
+     *    (Not optimized -> memory)
      */
 
     // Check if in-stock qty is sufficient
-    var _isInStock = function(product, order) {
-      // Return true if qty of product is available, otherwise false
+    const _isInStock = function (product, order) {
+      // Return true/false if qty of product is available
+      return product.stock_quantity >= order.qty;
+    }
+
+    // Calculates the total cost of the purchase
+    const _calculateTotalCost = function (product, order) {
+      // Return unit price times qty
+      return product.price * order.qty;
     }
 
     // Executes validated order processing
-    var _processOrder = function (product, order) {
+    const _processOrder = function (product, order) {
+      _executeQuery().then();
       // Calculate total cost with calculateTotalCost()
       // Decrease qty and add profit for product in database with updateProductQty()
       // Then, return order object with additional totalCost data
     }
 
-    // Calculates the total cost of the purchase
-    var _calculateTotalCost = function (product, order) {
-      // Return unit rate cost times qty
-    }
-
-    /**
-     *  Priviledged members
-     */
-
-    // Database query strings routed through this method (DRY optimization)
-    var _executeQuery = function (queryString) {
+    // Executes MySQL queries
+    const _executeQuery = async function (queryString, params = null) {
       try {
-        return new Promise( (resolve, reject) => {
-          // Query database for all products available for sale
-          _connection.query(queryString, (err, res, fields) => {
-            // Error handling
-            if (err) reject(err);
-            // If no error, resolve Promise
-            resolve(res);
-          });
+        // Query database for all products available for sale
+        return await _connection.query(queryString, params, (err, res, fields) => {
+          // Error handling
+          if (err) throw err;
+          // If no error, return results
+          return res;
         });
       } catch (error) {
         console.error(error);
@@ -89,20 +64,33 @@ class Database {
       // Return all products
       return await _executeQuery(queryString);
     }
-
-    // A bridge from public/prototypal members to access private members?
-    this.bridge = async function(command, ...params) {
-      return await command(params);
-    }
   }
 
   /**
    *  Public/prototypal methods
    */
 
+  // Establish connection with database
+  async connect() {
+    try {
+      await _connection.connect( err => {
+        if (err) throw err;
+        console.log(`MySQL connected as id: ${_connection.threadId}`);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   // Disconnect from database
+  async disconnect() {
+    await _connection.end();    
+  }  
+
+  
   disconnect() {
-    return _connection.end();
+    console.log(`disconnected from database`);
+    return 
   }
 
   // An async function that gets low inventory items based on given Qty threshold

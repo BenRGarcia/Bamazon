b593276b32e6465ab9cf52efd32aedfea20102f6
Ben Garcia <ben@seebenprogram.com>
2018-04-06 11:47:09 -0400

    Add DEFAULT product_sales

--- database/mockData.sql
+++ database/mockData.sql
@@ -8,22 +8,22 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(40) NOT NULL,
     department_name VARCHAR(40) NOT NULL,
-    price DECIMAL(10 , 2 ) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL,
-    product_sales DECIMAL(10 , 2 ) NOT NULL,
+    product_sales DECIMAL(10,2) DEFAULT 0.00,
     PRIMARY KEY (item_id)
 );
 
 CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(40) NOT NULL,
-    over_head_costs DECIMAL(10 , 2 ) NOT NULL,
+    over_head_costs DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (department_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
 VALUES  
-		("Steinway Model D Concert Grand Piano", "Instruments", 150000.00, 2, 150000.00),
+	("Steinway Model D Concert Grand Piano", "Instruments", 150000.00, 2, 150000.00),
     ("Harmonica", "Instruments", 2.00, 15, 30.00),
     ("Kazoo", "Instruments", 1.00, 50, 300.00),
     ("Sectional Sofa", "Furniture", 2000.00, 8, 4000.00),
@@ -36,7 +36,7 @@ VALUES
 
 INSERT INTO departments (department_name, over_head_costs)
 VALUES  
-		("Instruments", 10000.00),
+	("Instruments", 10000.00),
     ("Furniture", 2000.00),
     ("Laptops", 1000.00),
     ("Laptop Accessories", 5000.00);

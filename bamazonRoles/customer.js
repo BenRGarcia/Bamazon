b044a0c3624b814c866e9221c0ec9eaaa0b113f4
Ben Garcia <ben@seebenprogram.com>
2018-04-08 12:34:11 -0400

    reorganize code, remove hard coded 'new'

--- bamazonRoles/customer.js
+++ bamazonRoles/customer.js
@@ -2,69 +2,43 @@
 const inquirer = require('inquirer');
 const Database = require('../database/database.js');
 const { table } = require('table');
-
-// Create global access to DB and Customer object if customer instantiated
-let db, customer;
-
+// Create global access to DB if instantiated
+let db;
 // Define variables for ANSI text display styling
-let 
-  blueBG = '\u001b[44;1m',
-  blackBG = '\u001b[0m',
-  white = '\u001b[37m',
-  blue = '\u001b[34m';
-
+let blueBG = '\u001b[44;1m', blackBG = '\u001b[0m', white = '\u001b[37m', blue = '\u001b[34m';
+// Define questions with which to prompt customer
+const question1 = {
+  name: 'item_id',
+  type: 'input',
+  message: `Please type in the ${blue}ID${white} of the product you wish to buy:`,
+  validate: id => !isNaN(id)
+};
+const question2 = {
+  name: 'qty',
+  type: 'input',
+  message: `Please type in the ${blue}quantity${white} you wish to purchase:`,
+  validate: id => !isNaN(id)
+};
+// Aggregate questions to be asked
+const questions = [question1, question2];
 // Customer Class Constructor
 class Customer {
-  // When new customer instantiated...
   constructor() {
-    // ...instantiate database object
     db = new Database();
   }
-  // A function that will get all products from the DB
-  getProducts() { // Passing
-    return new Promise( (resolve, reject) => {
-      db.getAllProducts()
-        .then(res => {
-          resolve(res);
-        })
-        .catch(err => reject(err));
-    });
-  }
-  // Buy product
-  buyProduct(item_id, qty) { // Passing
-    return new Promise( (resolve, reject) => {
-      // Compose order object
-      let order = { item_id, qty };
-      // Place order
-      db.placeOrder(order)
-        .then(response => {
-          return response ? resolve(_orderSuccessful(response)) : resolve(_orderUnsuccessful(response));
-        })
-        .catch(err => reject(err));
-    });
+  initialize() {
+    initialize();
   }
 }
-// A function that clears the terminal, resets the cursor
-console.reset = () => process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
-// Handle when order is successful
-function _orderSuccessful(order) {
-  return `We have processed your order for ${order.qty} ${order.product}(s).\nYour total comes to $${order.totalCost.toFixed(2)}`;
-}
-// Handle when order is unsuccessful
-function _orderUnsuccessful() {
-  return `We're sorry, we don't have enough stock to fulfill that order.`;
-}
-// Instantiates customer object
+// Initializes customer
 function initialize() {
   // Clear the terminal
   console.reset();
-  // Instantiate new customer
-  customer = new Customer();
   // Retrieve all products from the DB
-  customer.getProducts()
-    .then( res => {
-      // Create product table with npm 'table' package
-      let products = [[`${blueBG} ID ${blackBG}`, `${blueBG} Product ${blackBG}`,`${blueBG} Price ${blackBG}`]];
+  getProducts()
+    .then(res => {
+      // Create product table (nested arrays) with npm 'table' package
+      let products = [[`${blueBG} ID ${blackBG}`, `${blueBG} Product ${blackBG}`, `${blueBG} Price ${blackBG}`]];
       res.forEach(product => {
         let tableRow = [];
         tableRow.push(product.item_id);
@@ -73,11 +47,7 @@ function initialize() {
         products.push(tableRow);
       });
       let tableConfig = {
-        columns: {
-          0: {
-            alignment: 'right'
-          }
-        }
+        columns: { 0: { alignment: 'right' } }
       }
       // Print *beautiful* table to the console
       console.log(`\n`);
@@ -90,34 +60,52 @@ function initialize() {
 // Asks which product to buy
 function promptPurchase(products) {
   inquirer
-    .prompt([
-      {
-        name: 'item_id',
-        type: 'input',
-        message: `Please type in the ${blue}ID${white} of the product you wish to buy:`,
-        validate: id => !isNaN(id)
-      },
-      {
-        name: 'qty',
-        type: 'input',
-        message: `Please type in the ${blue}quantity${white} you wish to purchase:`,
-        validate: id => !isNaN(id)
-      }
-    ])
-      // Place customer order
-      .then(purchase => {
-        console.log(`\nYour order is being placed...\n`);
-        // console.log(`You wish to purchase ${purchase.qty} of ID: ${purchase.item_id}`);
-        return customer.buyProduct(purchase.item_id, purchase.qty);
+    .prompt(questions)
+    // Place customer order
+    .then(purchase => {
+      return buyProduct(purchase.item_id, purchase.qty);
+    })
+    // Log results of order
+    .then(orderSuccess => {
+      console.log(orderSuccess);
+      console.log(`Thank you for shopping at ${blueBG}Bamazon${blackBG}!`);
+      db.disconnect();
+      return orderSuccess;
+    })
+    .catch(err => { throw err });
+}
+// A function that clears the terminal, resets the cursor
+console.reset = () => process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
+// A function that will get all products from the DB
+function getProducts() {
+  return new Promise((resolve, reject) => {
+    db.getAllProducts()
+      .then(res => {
+        resolve(res);
       })
-      // Log results of order
-      .then(orderSuccess => {
-        console.log(orderSuccess);
-        console.log(`Thank you for shopping at ${blueBG}Bamazon${blackBG}!`);
-        db.disconnect();
-        return orderSuccess;
+      .catch(err => reject(err));
+  });
+}
+// Buy product
+function buyProduct(item_id, qty) {
+  return new Promise((resolve, reject) => {
+    // Compose order object
+    let order = { item_id, qty };
+    // Place order
+    db.placeOrder(order)
+      .then(response => {
+        return response ? resolve(_orderSuccessful(response)) : resolve(_orderUnsuccessful(response));
       })
-      .catch( err => {throw err});
+      .catch(err => reject(err));
+  });
+}
+// Handle if order is successful
+function _orderSuccessful(order) {
+  return `We have processed your order for ${order.qty} ${order.product}(s).\nYour total comes to $${order.totalCost.toFixed(2)}`;
+}
+// Handle if order is unsuccessful
+function _orderUnsuccessful() {
+  return `We're sorry, we don't have enough stock to fulfill that order.`;
 }
 
 module.exports = Customer;

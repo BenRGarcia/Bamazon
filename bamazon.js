55fc4b29e97f92290f647233496b840e511f27a8
Ben Garcia <ben@seebenprogram.com>
2018-04-08 20:17:16 -0400

    tweak splash screen layout

--- bamazon.js
+++ bamazon.js
@@ -16,7 +16,7 @@ console.reset = () => process.stdout.write('\x1B[2J\x1B[0f\u001b[0;0H');
 const question = {
   name: 'role',
   type: 'list',
-  message: 'Welcome to Bamazon! Choose your role:',
+  message: 'Choose your role:',
   choices: ['Customer', 'Manager', 'Supervisor'],
   default: 0
 };
@@ -50,6 +50,7 @@ function determineRole() {
 fs.readFile('./bamazon.txt', 'utf8', (err, data) => {
   // Clear the terminal
   console.reset();
+  console.log(`Welcome to:\n`);
   console.log(data);
   console.log(`\n`);
   // Initialize app
